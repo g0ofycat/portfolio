c51d9a6202fc0d9f7ba27534f6e40f2579dc521e
g0ofycat <120335059+g0ofycat@users.noreply.github.com>
2025-01-31 17:07:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100 + 2;
+  var scrolled = (winScroll / height) * 100 + 4;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
